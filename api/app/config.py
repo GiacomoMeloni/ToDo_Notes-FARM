8a718bd2debb1772e54c67d663b7973df072cef0
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2023-05-15 22:04:21 +0200

    Added Settings class in config.py

--- api/app/config.py
+++ api/app/config.py
@@ -6,3 +6,5 @@ class Settings(BaseSettings):
     IP = os.environ.get('HOST_URL', '127.0.0.1')
     PORT = os.environ.get('HOST_PORT', 5000)
 
+
+settings = Settings()
--- api/application.py
+++ api/application.py
@@ -1,8 +1,6 @@
 import uvicorn
 
-from api.app.config import Settings
-
-settings = Settings()
+from api.app.config import settings
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host=settings.IP, port=settings.PORT)
