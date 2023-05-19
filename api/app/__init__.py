5a2515fa7f2224850106b205a9c355412d23f8bc
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2023-05-19 22:35:16 +0200

    Update init with Mongo client

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_utils.api_settings import get_api_settings
 
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from api.app.config import settings
 from api.app.router import todo_router
 from api.common.logger.setup import logger_setup
 
@@ -11,7 +15,10 @@ logger = logging.getLogger(__name__)
 
 
 get_api_settings.cache_clear()
-app = FastAPI(**get_api_settings().fastapi_kwargs)
+app = FastAPI(title=settings.PROJECT_NAME,
+              openapi_url=f"{settings.PREFIX}/openapi.json",
+              docs_url=f"{settings.PREFIX}/docs",
+              redoc_url=f"{settings.PREFIX}/redoc")
 
 app.add_middleware(CORSMiddleware,
                    allow_origins=["*"],
@@ -19,5 +26,16 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
+@app.on_event("startup")
+async def app_init():
+    """
+        initialize application services
+    :return:
+    """
+
+    db_client: AsyncIOMotorClient = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING)
+
+    await init_beanie(database=db_client,
+                      document_models=[])
 
 app.include_router(todo_router.router)
