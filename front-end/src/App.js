24e5fa2dfbb6830633bd4e3e285c9f185063058f
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2023-06-02 17:35:37 +0200

    Added webapp basic routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,13 @@
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+
 function App() {
-  return (
-    <div>Hello World!</div> 
-  );
+  return <Router>
+      <Routes>
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+      </Routes>
+    </Router>
+  
 }
 
 export default App;
