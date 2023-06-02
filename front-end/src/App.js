9c4410418b448bbb0ef2c7fb33ff6b45085e43bd
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2023-06-02 18:31:18 +0200

    Added Login Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,10 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import { Login } from './components/auth/login';
 
 function App() {
   return <Router>
       <Routes>
-        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/login" element={<h1><Login></Login></h1>} />
         <Route path="/register" element={<h1>Register</h1>} />
       </Routes>
     </Router>
