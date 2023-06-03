471f41031cb6ffb4a11228de131382c56f62e8c1
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2023-06-03 14:46:56 +0200

    Added Register page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import { Login } from './components/auth/login';
+import { Register } from './components/auth/register'
 
 function App() {
   return <Router>
       <Routes>
         <Route path="/login" element={<h1><Login/></h1>} />
-        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/register" element={<h1><Register/></h1>} />
       </Routes>
     </Router>
   
---
+++ front-end/src/components/auth/register.jsx
@@ -0,0 +1,103 @@
+import { Flex, FormControl, FormErrorMessage, Heading, Input, useColorModeValue, Button } from "@chakra-ui/react";
+import { useForm } from 'react-hook-form'
+import { useNavigate } from "react-router-dom";
+import { ThemeToggler } from "../theme/themeToggler";
+
+export const Register = () => {
+    const {
+        handleSubmit,
+        register,
+        formState: {errors, isSubmitting}
+    } = useForm();
+
+
+    const navigate = useNavigate();
+
+    const onSubmit = (values) => {
+        console.log(values);
+    }
+
+    return (
+    
+    <Flex height="100vh" align="center" justifyContent="center">
+        <Flex 
+        direction="column" 
+        alignItems="center" 
+        background={useColorModeValue('gray.100', 'gray.700')}
+        p={12}
+        rounded={6} 
+        >
+            <Heading mb={6}>Login</Heading>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl isInvalid={errors.email}>
+                    <Input
+                    placeholder="email"
+                    background={useColorModeValue('gray.300', 'gray.600')}
+                    type="email"
+                    size="lg"
+                    mt={6}
+                    {...register("email", {
+                        required: "This is a required field"
+                    })}
+                    />
+                    <FormErrorMessage>
+                        {errors.email && errors.email.message}
+                    </FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={errors.username}>
+                    <Input
+                    placeholder="username"
+                    background={useColorModeValue('gray.300', 'gray.600')}
+                    type="username"
+                    size="lg"
+                    mt={6}
+                    {...register("username", {
+                        required: "This is a required field",
+                        minLength: {value: 5, message: "Username must be at least 5 characters"},
+                        maxLength: {value: 24, message: "Username must be at most 24 characters"}
+                    })}
+                    />
+                    <FormErrorMessage>
+                        {errors.username && errors.username.message}
+                    </FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={errors.password}>
+                    <Input
+                    placeholder="password"
+                    background={useColorModeValue('gray.300', 'gray.600')}
+                    type="password"
+                    size="lg"
+                    mt={6}
+                    {...register("password", {
+                        required: "This is a required field",
+                        minLength: {value: 5, message: "Password must be at least 5 characters"},
+                        maxLength: {value: 24, message: "Username must be at most 24 characters"}
+                    })}
+                    />
+                    <FormErrorMessage>
+                        {errors.password && errors.password.message}
+                    </FormErrorMessage>
+                </FormControl>
+                
+                <Button 
+                isLoading={isSubmitting}
+                loadingText="Logging in..."
+                width="100%"
+                colorScheme="green"
+                variant="outline"
+                mt={6}
+                mb={6}
+                type="submit">Register</Button>
+            </form>
+            <ThemeToggler  showLabel={true}/>
+
+            <Button onClick={()=>navigate("/login", {replace: true})} 
+            width="100%"
+            colorScheme="gray"
+            variant="outline"
+            mt={6}>Login Instead</Button>
+        </Flex>
+    </Flex>
+);}
