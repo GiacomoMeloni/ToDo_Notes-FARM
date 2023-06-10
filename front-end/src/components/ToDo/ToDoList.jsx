f44fe95c7876bbcd344280c50ab8685f53624b8d
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2023-06-10 10:22:15 +0200

    Integrating ToDo List

--- front-end/src/components/ToDo/ToDoList.jsx
+++ front-end/src/components/ToDo/ToDoList.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+import { Box, Center, Container, Spinner } from "@chakra-ui/react"
+import AxiosInstance from "../../services/auth_service";
+
 export const ToDoList = () => {
+    const [todos, setTodos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const isMounted = useRef(false);
+    
+    useEffect(() => {
+        if (isMounted) return;
+        fetchToDo();
+        isMounted.current = true;
+    }, []);
+
+    const fetchToDo = async () => {
+        setLoading(true);
+        AxiosInstance.get("/todo")
+        .then((res) => {
+            setTodos(res.data);
+        }).catch((error) => {
+            console.error(error);
+        }).finally(() => {
+            setLoading(false);
+        });
+    }
+    
     return (
-        <>
-            ToDoList
-        </>
+        <Container mt={9}>
+            {loading ? (
+                <Center mt={6}>
+                    <Spinner
+                        thickness="4px"
+                        speed="0.65s"
+                        emptyColor="green.200"
+                        color="green.500"
+                        size="xl"
+                    />
+                </Center>
+            ) : ( 
+                <Box mt={6}>
+                    { todos?.map((todo) => (
+                        <TodoCard todo={todo} key={todo.id}/>
+                    ))}
+                </Box>
+            )}
+             {/* Add ToDo modal  */}
+        </Container>
     );
 }
