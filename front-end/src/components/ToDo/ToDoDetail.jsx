ffc8af58bb37f78f7321651856915342fd18d343
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2023-06-12 23:37:18 +0200

    Added logic on ToDo Details

--- front-end/src/components/ToDo/ToDoDetail.jsx
+++ front-end/src/components/ToDo/ToDoDetail.jsx
@@ -1,7 +1,59 @@
+import { useState, useRef, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom"; 
+import AxiosInstance from "../../services/auth_service";
+import { Button, Center, Container, Spinner } from "@chakra-ui/react"
+
 export const ToDoDetail = () => {
+    const [todo, setTodo] = useState({});
+    const [loading, setLoading] = useState(true);
+    const isMounted = useRef(false);
+    const navigate = useNavigate();
+    const { todoId } = useParams()
+
+    useEffect(() => {
+        if (isMounted.current) return;
+        fetchTodo();
+        isMounted.current = true;
+    }, [todoId]);
+
+    const fetchTodo = () => {
+        setLoading(true)
+        AxiosInstance.get(`/todo/${todoId}`)
+        .then((response) => {
+            setTodo(response.data)
+        })
+        .catch((error) => console.error(error))
+        .finally(() => {
+            setLoading(false);
+        })
+    }
+
+    if (loading) {
+        return (
+            <Container mt={6}>
+                <Center mt={6}>
+                    <Spinner
+                        thickness="4px"
+                        speed="0.65s"
+                        emptyColor="green.200"
+                        color="green.500"
+                        size="xl"
+                    />
+                </Center>
+            </Container>
+            
+        )
+    }
+    
     return (
         <>
-            ToDo Details
+            <Container mt={6}>
+                <Button 
+                colorScheme="gray"
+                onClick={() => navigate('/', {replace: true})}>
+                    Back
+                </Button>
+            </Container>
         </>
     );
 }
